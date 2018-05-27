55246083da92b7adf95772a1b62c6d416b87d158
Jenisha Patel <jenisha.patel@mail.mcgill.ca>
2018-05-26 21:19:51 -0400

    update

--- model.py
+++ model.py
@@ -117,52 +117,56 @@ else:
     input_shape = (img_width, img_height, 3)
 
 
+def model_def():
+	model = Sequential()
 
-model = Sequential()
+	model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+	model.add(Activation('relu'))
 
-model.add(Activation('relu'))
-
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-
-
-model.add(Conv2D(32, (3, 3)))
-
-model.add(Activation('relu'))
-
-model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
 
 
 
-model.add(Conv2D(64, (3, 3)))
+	model.add(Conv2D(32, (3, 3)))
 
-model.add(Activation('relu'))
+	model.add(Activation('relu'))
 
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-
-
-model.add(Flatten())
-
-model.add(Dense(64))
-
-model.add(Activation('relu'))
-
-model.add(Dropout(0.5))
-
-model.add(Dense(1))
-
-model.add(Activation('sigmoid'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
 
 
+
+	model.add(Conv2D(64, (3, 3)))
+
+	model.add(Activation('relu'))
+
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+
+	model.add(Flatten())
+
+	model.add(Dense(64))
+
+	model.add(Activation('relu'))
+
+	model.add(Dropout(0.5))
+
+	model.add(Dense(1))
+
+	model.add(Activation('sigmoid'))
+
+	return model
+
+
+model = model_def()
 
 model.compile(loss='binary_crossentropy',
 
               optimizer='rmsprop',
 
               metrics=['accuracy'])
+
 
 
 
